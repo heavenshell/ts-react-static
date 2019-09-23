f07eab11ea49a712126aa585c069ab34b0f8c2f7
Shinya Ohyanagi <heavenshell.jp@gmail.com>
2019-09-23 23:22:40 +0900

    Fix script

--- scripts/reg.sh
+++ scripts/reg.sh
@@ -1,33 +1,49 @@
 #!/bin/sh
 
 function prepare () {
+  echo "Prepare for reg-cli"
   mkdir -p ./dist/storybook
   rm -rf ./dist/__regressions__
-  mkdir -p ./dist/__regressions__/{expected,actual}
+  mkdir -p ./dist/__regressions__/{expected,actual,tmp}
   yarn build-storybook -o ./dist/storybook
   yarn screenshot -o ./dist/__regressions__/actual
+  echo "zisui finished"
 }
 
 function download () {
   echo "wget expected file"
-  wget https://heavenshell.netflify.com/__regressions__/expected/files.tar.gz
+  wget https://heavenshell.netlify.com/__regressions__/expected/files.tar.gz
   if [ -e files.tar.gz ]; then
-    mv files.tar.gz ./dist/__regressions__/expected/files.tar.gz
+    mv files.tar.gz ./dist/__regressions__/tmp/files.tar.gz
     echo "unzip file.tar.gz"
-    tar xzf ./dist/__regressions__/expected/files.tar.gz ./dist/__regressions__/exptected
+    current=`pwd`
+    cd ./dist/__regressions__/tmp
+    tar xzfv files.tar.gz
+    cd $current
+    mv ./dist/__regressions__/tmp/expected ./dist/__regressions__/exptected/.
+    echo "move exptected to ./dist/__regressions__/exptected"
   fi
 }
 
 function run_reg () {
+  echo "Run reg-cli"
   reg-cli ./dist/__regressions__/actual ./dist/__regressions__/expected ./dist/__regressions__/reports/ -R ./dist/__regressions__/report.html
   mv reg.json ./dist/__regressions__/.
+  rm -rf ./dist/__regressions__/tmp/*
+  mv ./dist/__regressions__/actual ./dist/__regressions__/tmp/expected
+  echo "./dist/__regressions__/actual to ./dist/__regressions__/tmp/exptected"
 }
 
-
 function compress () {
-  if [ -d ./dist/__regressions__/expected ]; then
+  if [ -d ./dist/__regressions__/tmp ]; then
     echo "compress files and move to exptected"
-    tar czf ./dist/__regressions__/expected/files.tar.gz ./dist/__regressions__/actual
+    current=`pwd`
+    cd ./dist/__regressions__/tmp
+    echo `pwd`
+    tar czf files.tar.gz ./expected
+    cd $current
+    mv ./dist/__regressions__/tmp/files.tar.gz ./dist/__regressions__/expected/.
+    echo "move files.tar.gz to ./dist/__regressions__/exptected"
   fi
 }
 
