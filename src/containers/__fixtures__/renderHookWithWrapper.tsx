6627288a3add473e3c7182fbd89fea9cae228f45
Shinya Ohyanagi <heavenshell.jp@gmail.com>
2021-01-09 19:10:10 +0900

    Update deps (#231)

--- src/containers/__fixtures__/renderHookWithWrapper.tsx
+++ src/containers/__fixtures__/renderHookWithWrapper.tsx
@@ -1,12 +1,15 @@
 import * as React from 'react'
-import { renderHook, RenderHookOptions } from '@testing-library/react-hooks'
+import { renderHook } from '@testing-library/react-hooks'
 
 import TestProvider, { Props } from './TestProvider'
 
 const renderHookWithWrapper = <P, R>(
   callback: (props: P) => R,
   { paths, search, hash }: Props,
-  renderHookOptions?: Omit<RenderHookOptions<P>, 'wrapper'>
+  renderHookOptions?: {
+    initialProps?: P
+    wrapper?: React.ComponentType<P>
+  }
 ) =>
   renderHook(callback, {
     wrapper: ({ children }) => (
